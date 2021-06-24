bbf855360cc5e4ffc9b381431825e1d42c81ca0b
disaisoft <disaisoft@gmail.com>
2021-06-23 21:18:43 -0500

    creación de arquitectura AO

---
+++ alternativas/src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import Header from './components/HeaderComponent/Header';
+
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <React.Fragment>
+      <Header />      
+      
+    </React.Fragment>
+  );
+}
+export default App;
+
